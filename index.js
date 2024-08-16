af2a079e62428260aced571c51c4fba3754d9de4
Aditya-sisrtd <aditya.prakash@sisrtd.com>
2024-08-16 12:30:33 +0530

    learning Document Object Model in Java script

---
+++ index.js
@@ -0,0 +1,29 @@
+// console.log(window);
+// // console.log('hello');
+// // window.console.log('hello_window');
+// console.dir(window);
+// console.log('hello');
+// console.dir("hello");
+// let heading = document.getElementById('heading');
+// console.dir(heading);
+// let box = document.getElementsByClassName('box');
+// console.dir(box);
+// let heading = document.getElementsByTagName('h1');
+// console.dir(heading);
+// let obj = document.querySelector('div');
+// console.dir(obj);
+// let objects = document.querySelectorAll('div');
+// console.dir(objects);
+// let heading = document.querySelector("#heading");
+// console.dir(heading);
+// let firstEl = document.querySelector("#heading");
+// console.dir(firstEl);
+//console.log(firstEl.innerHTML);
+// let h2 = document.querySelector('h2');
+// console.dir(h2.innerText);
+// h2.innerText = h2.innerText + 'from apna college students';
+// console.dir(h2.innerText);
+let divs = document.querySelectorAll('.box');
+divs[0].innerText = 'new Unique Value one';
+divs[1].innerText = 'new Unique Value two'
+divs[2].innerText = 'new Unique Value three'
